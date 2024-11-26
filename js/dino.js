491ed64dd34d23b2d054d8de1b08a4690da17dfd
Mike <mg.korop@gmail.com>
2024-11-26 20:36:22 +0200

    made game area start and update

--- js/dino.js
+++ js/dino.js
@@ -1,22 +1,65 @@
-const body = document.querySelector('body');
+const gameArea = {
+    // створити елемент canvas
+    canvas: document.createElement('canvas'),
 
-// створити елемент canvas всередині <body> 
-const canvas = document.createElement('canvas');
-body.appendChild(canvas);
+    // функція, що запускає гру
+    start: function () {
+        // задати розміри canvas
+        this.canvas.width = 720;
+        this.canvas.height = 240;
 
-// задати розміри елементу canvas
-canvas.width = 720;
-canvas.height = 241;
+        // тимчасовий кольоровий індикатор
+        this.canvas.style.backgroundColor = 'fuchsia';
 
-// тимчасовий індикатор
-canvas.style.backgroundColor = 'fuchsia';
+        // додати canvas до <body>
+        document.body.appendChild(this.canvas);
 
-// контекст рендерінга (власне для малювання гри)
-const ctx = canvas.getContext('2d');
+        // отримати контекст рендерінгу
+        this.ctx = this.canvas.getContext('2d');
 
-const dinoImg = new Image();
-dinoImg.src = './img/dino-run-1.png';
+        // задати інтервал оновлення екрану (у мілісекундах)
+        this.interval = setInterval(updateGameArea, 20);
 
-dinoImg.onload = () => {
-    ctx.drawImage(dinoImg, 0, 0, dinoImg.width, dinoImg.height);
+        // код для відслідковування натисків клавіш
+        window.addEventListener('keydown', (e) => {
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = true;
+        })
+
+        window.addEventListener('keyup', (e) => {
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = false;
+        })
+    },
+
+    // функція для стирання екрану
+    clear: function () {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
+// функція для оновлення гри
+const updateGameArea = function () {
+    gameArea.clear();
+    // gameArea.ctx.fillRect(Math.random() * gameArea.canvas.width, Math.random() * gameArea.canvas.height, 30, 30);
+    gameArea
+}
+
+const dino = {
+    image: new Image(src='./img/dino-run-1.png'),
+    originY: 100,
+    posY: 0,
+    spdY: 0,
+    move: function () {
+        this.posY += this.spdY;
+        if (!(this.posY == this.originY)) {
+            
+        }
+    },
+
+    jump: function () {
+        this.spdY = 10;
+    }
+}
+
+// запустити гру
+gameArea.start();
