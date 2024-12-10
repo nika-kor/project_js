dbcbd81757d785853eaf12d5734d125f54a28e9f
Nika <nika.koromyslova@gmail.com>
2024-12-10 16:56:06 +0200

    slider-almost-done

--- js/filter.js
+++ js/filter.js
@@ -1,47 +1,8 @@
-//Верстка секції з карточками інтерактивної частини сайту, лише пуста картка. Контент карток створює кожен виконавець своєї гри окремо за допомогою js. Кожна картка повинна мати унікальний ідентифікатор. Написати скрипт який буде створювати картки по кількості завдань, використовуючи масив об'єктів, де будуте зберігатися вся інформаці. Реалізувати фільтр інтерактивних завдань по категорії зі списку, який розгортається при натискані на 'Інтерактив'
+// // // фільтер сайту 
 
 
-const numercial = document.querySelectorAll('.numercial');
-const game = document.querySelectorAll('.game');
-const acquaintance = document.querySelectorAll('.acquaintance');
-
-
-const numercialBtn = document.querySelector('.numercial-btn');
-const gameBtn = document.querySelector('.game-btn');
-const acquaintanceBtn = document.querySelector('.acquaintance-btn');
-
-const listOfBtns = document.querySelector('.nav-list-submenu');
-
-
-function filter(e) {
-    let filtredSection;
-
-    switch (e.target.class) {
-        case 'numercial-btn':
-            filtredSection = numercial;
-            break;
-        
-    }
-    
+window.toggleOn = (e) => {
+    document.querySelectorAll('section').forEach(el => el.style.display = 'none');
+    document.querySelectorAll(`.${e}`).forEach(el => el.style.display = 'block');
+    console.log(e)
 }
-// function filterForGame() {
-//     numerial.forEach(el => el.style.display = 'none');
-//     game.forEach(el => el.style.display = 'flex');
-//     acquaintance.forEach(el => el.style.display = 'none');
-// }
-// function filterForAcquaintance() {
-//     numerial.forEach(el => el.style.display = 'none');
-//     game.forEach(el => el.style.display = 'none');
-//     acquaintance.forEach(el => el.style.display = 'flex');
-// }
-
-// numercialBtn.addEventListener('click', filterForNumercial);
-// gameBtn.addEventListener('click', filterForGame);
-// acquaintanceBtn.addEventListener('click', filterForAcquaintance);
-
-listOfBtns.addEventListener('click', filter);
-
-
-
-
-
--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 const slidesFilling = [
     {
-        img: 'https://math-media.byjusfutureschool.com/bfs-math/2022/07/04185628/Asset-1-8-300x300.png', 
+        img: '../image/nika.jpg', 
         name: 'nika', 
         work: 'Інформація про роботу, яку він/вона виконав/ла'
     },
@@ -11,12 +11,12 @@ const slidesFilling = [
         work: 'Інформація про роботу, яку він/вона виконав/ла'
     },
     {
-        img: 'https://img.freepik.com/premium-vector/decorative-ornamental-square-frame-border-with-geometric-lines-nouveau-style-design_1226483-13552.jpg?semt=ais_hybrid', 
+        img: '../image/misha.jpg', 
         name: 'misha', 
         work: 'Інформація про роботу, яку він/вона виконав/ла'
     },
     {
-        img: 'https://www.freeiconspng.com/thumbs/square-png/blue-square-image-3.png', 
+        img: '../image/vika.jpg', 
         name: 'vika', 
         work: 'Інформація про роботу, яку він/вона виконав/ла'
     },
@@ -29,9 +29,11 @@ const slidesFilling = [
 
 const section = document.querySelector('.acquaintance');
 
+/* <svg width="24" height="16"><use href="./symbol-defs.svg#burger"></use></svg> */
+
 const title = document.createElement('h2');
 title.textContent = 'Наша команда';
-title.classList.add('our-team-h2');
+title.classList.add('our-team-title');
 
 const slider = document.createElement('div');
 slider.classList.add('our-team-slider');
@@ -39,11 +41,20 @@ slider.classList.add('our-team-slider');
 
 const btnPrev = document.createElement('button');
 btnPrev.classList.add('our-team-btn-prev');
-btnPrev.textContent = '«';
+
+const svgPrev = document.createElement('svg');
+svgPrev.setAttribute('width', '24');
+
+const usePrev = document.createElement('use');
+usePrev.setAttribute('href', '../symbol-defs.svg#arrowLeft');
+
+svgPrev.append(usePrev);
+btnPrev.append(svgPrev);
+// btnPrev.textContent = '«';
 
 const btnNext = document.createElement('button');
 btnNext.classList.add('our-team-btn-next');
-btnNext.textContent = '»';
+// btnNext.textContent = '»';
 
 
 // const slidesWrapper = document.createElement('div');
@@ -73,12 +84,40 @@ const slidesWrapper = slidesFilling.map((slidesFilling) => `<div class="slide"><
 listOfImages.insertAdjacentHTML('afterbegin', slidesWrapper);
 
 
+const buttonsOnBottomList = document.createElement('div');
+buttonsOnBottomList.classList.add('our-team-buttons-lines');  
+
+const buttonsOnBottomFirst = document.createElement('button');
+buttonsOnBottomFirst.classList.add('line-btn');
+
+const buttonsOnBottomSecond = document.createElement('button');
+buttonsOnBottomSecond.classList.add('line-btn');
+
+const buttonsOnBottomThird = document.createElement('button');
+buttonsOnBottomThird.classList.add('line-btn');
+
+const buttonsOnBottomFourth = document.createElement('button');
+buttonsOnBottomFourth.classList.add('line-btn');
+
+const buttonsOnBottomFifth = document.createElement('button');
+buttonsOnBottomFifth.classList.add('line-btn');
+
+
+slider.append(buttonsOnBottomList);
+buttonsOnBottomList.append(buttonsOnBottomFirst, buttonsOnBottomSecond, buttonsOnBottomThird, buttonsOnBottomFourth, buttonsOnBottomFifth);
 
 
 
-// const btnPrev = document.querySelector(".prev");
-// const btnNext = document.querySelector(".next");
-// const listOfImages = document.querySelector(".slides");
+// move slides when pressing buttons
+
+
+
+
+
+
+
+
+
 let currentSlide = 0;
 
 
@@ -95,18 +134,30 @@ function moveSlides(position) {
     currentSlide = position;
 	
 	listOfImages.style.transform = `translateX(-${position * 800}px)`;
+
+    let dots = document.getElementsByClassName("line-btn");
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    dots[currentSlide].classList.add("active");
 }
 
 
 
 
 
-// btnPrev.addEventListener('click',()=> moveSlides(currentSlide -1));
-// btnNext.addEventListener('click',()=> moveSlides(currentSlide +1));
-
-
 btnPrev.addEventListener('click', function () {moveSlides(currentSlide -1)});
 btnNext.addEventListener('click', function() {moveSlides(currentSlide +1)});
+
+
+
+
+
+buttonsOnBottomFirst.addEventListener('click', function() {moveSlides(0)});
+buttonsOnBottomSecond.addEventListener('click', function() {moveSlides(1)});
+buttonsOnBottomThird.addEventListener('click', function() {moveSlides(2)});
+buttonsOnBottomFourth.addEventListener('click', function() {moveSlides(3)});
+buttonsOnBottomFifth.addEventListener('click', function() {moveSlides(4)});
 
 
 
