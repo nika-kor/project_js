20d2df3ad990716a6bfcf7bd6108ca393aafbc67
Nika <nika.koromyslova@gmail.com>
2024-12-14 11:19:28 +0200

    anton merge

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,26 @@
 const slidesFilling = [
     {
-        img: './image/nika.jpg', 
+        img: './img/nika.jpg', 
         name: 'Nika', 
         work: 'Team lead, слайдер нашої команди та два фільтри'
     },
     {
-        img: './image/misha.jpg', 
+        img: './img/misha.jpg', 
         name: 'Misha', 
         work: 'Гра динозаврик та футбол, секція перевірити рік'
     },
     {
-        img: './image/vika.jpg', 
+        img: './img/vika.jpg', 
         name: 'Vika', 
         work: 'модалка, секця вгадай число яке загадав комп\'ютер та введи три числа'
     },
     {
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvDKV7L91pePEUNFbnS1mOzlWvsAQGY_S9RhB3vvd49G7TNBPCJpeytWgdt69s7IxSzFk&usqp=CAU', 
+        img: './img/anton.jpg', 
         name: 'Anton', 
         work: 'Верстка сайту, та секція камінь ножиці папір'
     },
     {
-        img: 'https://pngimg.com/uploads/square/square_PNG67.png', 
+        img: './img/alex.jpg', 
         name: 'Oleksiy', 
         work: 'Калькулятор та калькулятор часу'
     },
